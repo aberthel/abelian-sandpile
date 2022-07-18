e8fbb3a2c092291af015782c02ac5e9c0bf42d5d
aberthel <ana.h.berthel@gmail.com>
2022-07-18 12:31:55 -0400

    basic spill function added

--- main.py
+++ main.py
@@ -3,13 +3,7 @@
 import tkinter as tk
 import numpy as np
 from PIL import Image, ImageTk, ImagePalette
-
-### VARIABLES ###
-
-#spill_pattern
-
-max_slope = 3
-#spill_queue
+import time
 
 class Sandbox:
     def __init__(self, height, width):
@@ -17,31 +11,73 @@ class Sandbox:
         self.width = width
 
         self.bucket = 1
-
+        self.max_slope = 3 #i.e. if cell has a slope of 4, then add it to the spill queue
         self.array = np.zeros((height, width))
+
+        self.spill_pattern = np.array([[0,1,0], [1,0,1], [0,1,0]])
+
+        self.spill_queue = [] #starts out empty
 
 
     def place_sand(self, tup):
 
         self.array[tup[1], tup[0]] += self.bucket
-        #TODO: check for spilling, initiate spill if necessary
+        # NOTE: will this work when multiple scripts are used???
+        update_image()
+        if self.array[tup[1], tup[0]] > self.max_slope:
+
+            if not tup in self.spill_queue:
+                self.spill_queue.append(tup)
+                self.spill()
+
+    def spill(self):
+        # TODO: lock place_sand input?
+        while self.spill_queue:
+            tup = self.spill_queue.pop(0)
+            grains_spilled = int(self.array[tup[1], tup[0]]/np.sum(self.spill_pattern))
+            self.array[tup[1], tup[0]] = self.array[tup[1], tup[0]] % np.sum(self.spill_pattern)
+
+            mid_i = int(self.spill_pattern.shape[1]/2)
+            mid_j = int(self.spill_pattern.shape[0]/2)
+
+            for i in range(self.spill_pattern.shape[1]):
+                for j in range(self.spill_pattern.shape[0]):
+                    if self.spill_pattern[i, j] == 1:
+                        self.array[tup[1] - mid_i + i, tup[0] - mid_j + j] += grains_spilled
+                        #NOTE: animation of spilling doesn't happen. Force computer to wait?
+                        update_image()
+                        if self.array[tup[1] - mid_i + i, tup[0] - mid_j + j] > self.max_slope:
+                            self.spill_queue.append((tup[0] - mid_j + j, tup[1] - mid_i + i))
+
+
+
+
 
 # converts sandbox array into an image
 class ImageBuilder:
     def __init__(self):
         self.zoom = 10
-        self.palette = [np.array([252,251,237]), np.array([197,245,152]), np.array([133,191,78]), np.array([83,133,37]), np.array([89,64,13])]
 
-    def to_image(self, array):
-        image_array = np.zeros((array.shape[0], array.shape[1], 3), dtype=np.uint8)
+        #TODO: make sure palette has at least as many options as max_slope + 1 when it's set
+        self.palette = [np.array([252,251,237]), np.array([197,245,152]), np.array([133,191,78]), np.array([83,133,37]), np.array([89,64,13])]
+        self.overflow_color = np.array([201, 30, 18])
+
+
+    def to_image(self, sandbox):
+
+        image_array = np.zeros((sandbox.array.shape[0], sandbox.array.shape[1], 3), dtype=np.uint8)
 
         #TODO: better way to do this?
-        for x in range(array.shape[0]):
-            for y in range(array.shape[1]):
-                image_array[x, y] = self.palette[int(array[x, y])]
+        for x in range(sandbox.array.shape[0]):
+            for y in range(sandbox.array.shape[1]):
+
+                if sandbox.array[x, y] <= sandbox.max_slope:
+                    image_array[x, y] = self.palette[int(sandbox.array[x, y])]
+                else:
+                    image_array[x, y] = self.overflow_color
 
 
-        img1 = Image.fromarray(image_array, mode="RGB").resize((array.shape[0] * self.zoom, array.shape[1] * self.zoom), resample = Image.NEAREST)
+        img1 = Image.fromarray(image_array, mode="RGB").resize((sandbox.array.shape[0] * self.zoom, sandbox.array.shape[1] * self.zoom), resample = Image.NEAREST)
 
         #TODO: zoom?
 
@@ -69,10 +105,10 @@ def open_settings(event):
     settings_label = tk.Label(master=top_frame, text="Settings")
     height_label = tk.Label(master=top_frame, text="Sandbox Height")
     height_entry = tk.Entry(master=top_frame, width=10)
-    height_entry.insert(0, str(sb_height))
+    height_entry.insert(0, str(sandbox.height))
     width_label = tk.Label(master=top_frame, text="Sandbox Width")
     width_entry = tk.Entry(master=top_frame, width=10)
-    width_entry.insert(0, str(sb_width))
+    width_entry.insert(0, str(sandbox.width))
 
     settings_label.pack(side=tk.TOP)
     height_label.pack(side=tk.LEFT)
@@ -113,13 +149,13 @@ def open_save(event):
 
 def update_image():
     global current_image
-    current_image = ib.to_image(sandbox.array)
+    current_image = ib.to_image(sandbox)
     main_canvas.itemconfig(image_container, image=current_image)
 
 def place_sand(event):
     sandbox.place_sand(ib.im_to_coords(event.x, event.y))
 
-    update_image()
+    #update_image()
 
 
 
@@ -157,7 +193,7 @@ drawing_frame.pack()
 
 # draw image from sandbox array
 # TODO: replace with function later in order to define palette, pixel size, etc.
-current_image = ib.to_image(sandbox.array)
+current_image = ib.to_image(sandbox)
 image_container = main_canvas.create_image(0, 0, image=current_image, anchor="nw")
 
 main_window.mainloop()
